b8d00e6579f651593dfbb481ade4063c9ee86f8a
Oleksii Kuzmenko <alexey.kuzmenko1101@gmail.com>
2023-10-11 11:26:41 +0300

    feat: added specific modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectModule } from './project/project.module';
+import { PageModule } from './page/page.module';
+import { SkillModule } from './skill/skill.module';
+import { ContactModule } from './contact/contact.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectModule, PageModule, SkillModule, ContactModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
