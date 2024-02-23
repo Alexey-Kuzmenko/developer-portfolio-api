653002a89d2a88a01fc0dd7e50967a9c98cfc818
Oleksii Kuzmenko <alexey.kuzmenko1101@gmail.com>
2024-02-23 17:36:23 +0200

    feat: added notification in Telegram about the authorization
    When the user sing in or signs up, the Telegram bot will send a message about
    this action.

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -3,6 +3,8 @@ import { MessageDto } from './dto/message.dto';
 import { Telegraf } from 'telegraf'
 import { TelegramOptions } from './telegram.interface';
 import { TELEGRAM_MODULE_OPTIONS } from './telegram.constants';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+import { format } from 'date-fns';
 
 @Injectable()
 export class TelegramService {
@@ -14,16 +16,38 @@ export class TelegramService {
         this.options = options
     }
 
-    generateMessage(dto: MessageDto): string {
-        return `Name: ${dto.name}`
-            + '\n'
-            + 'Email:' + ' ' + '`' + dto.email + '`'
-            + '\n'
-            + '\n'
-            + `Message: ${dto.message}`
+    generateMessage(dto: MessageDto | AuthDto, messageType: 'order' | 'register' | 'login'): string {
+        if (messageType === 'order' && 'message' in dto) {
+            return `Name: ${dto.name}`
+                + '\n'
+                + 'Email:' + ' ' + '`' + dto.email + '`'
+                + '\n'
+                + '\n'
+                + `Message: ${dto.message}`
+        }
+
+        if (messageType === 'register' && 'email' in dto) {
+            return 'Attention! New user registered.'
+                + '\n'
+                + 'User email:' + ' ' + '`' + dto.email + '`'
+                + '\n'
+                + `Time of registration: ${format(new Date(), 'pp dd MMM yyyy')}`
+        }
+
+        if (messageType === 'login' && 'email' in dto) {
+            return 'Attention! Detected login in the system.'
+                + '\n'
+                + 'User email:' + ' ' + '`' + dto.email + '`'
+                + '\n'
+                + `Time of registration: ${format(new Date(), 'pp dd MMM yyyy')}`
+        }
     }
 
     async sendMessage(message: string) {
         this.bot.telegram.sendMessage(this.options.chatId, message, { parse_mode: 'Markdown' })
+    }
+
+    async sendAuthMessage(message: string) {
+        this.bot.telegram.sendMessage(this.options.botChatId, message, { parse_mode: 'Markdown' })
     }
 }
