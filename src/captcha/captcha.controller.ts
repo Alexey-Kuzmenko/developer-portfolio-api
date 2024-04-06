b1279666000e70dfb10f9c1227fc0c3e41d61572
Oleksii Kuzmenko <alexey.kuzmenko1101@gmail.com>
2024-04-06 12:24:57 +0300

    fix: added ApiKeyAuthGuard for captcha controller

--- src/captcha/captcha.controller.ts
+++ src/captcha/captcha.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CaptchaDto } from './dto/captcha.dto';
 import { CaptchaResponseDto } from './dto/captcha-response.dto';
 import { CaptchaService } from './captcha.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiKeyAuthGuard } from 'src/auth/guards/api-key.guard';
 
 @ApiTags('captcha')
 @Controller('captcha')
 export class CaptchaController {
     constructor(private readonly captchaService: CaptchaService) { }
 
+    @UseGuards(ApiKeyAuthGuard)
     @UsePipes(new ValidationPipe())
     @HttpCode(HttpStatus.OK)
     @Post('verify')
